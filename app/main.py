512a976891126be9018a459cbfd83aa43017b24d
Ajara Oyinloye <lawalajara1@gmail.com>
2024-04-24 12:11:57 +0000

    working with SQLALchemy

--- app/main.py
+++ app/main.py
@@ -99,14 +99,15 @@ def delete_post(id: int):
 
 @app.put('/posts/{id}')
 def update_post(id: int, Post: post):
-    cursor.execute("""UPDATE post SET title = %s, content = %s, published = %s Returning *""", 
-                   (Post.title, Post.content, Post.published))
+    cursor.execute("""UPDATE post SET title = %s, content = %s, published = %s WHERE id = %s Returning *""", 
+                   (Post.title, Post.content, Post.published, str(id)))
     updated_post = cursor.fetchone()
+    conn.commit()
 
     if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with the id {id} does not exist")
-    return {'data': update_post}
+    return {'data': updated_post}
 
 
    
